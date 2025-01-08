fc9984a0eddfe24b5533ca4a5c641d2c0119ceeb
charles dazel <charlesdazel2019@gmail.com>
2025-01-08 08:52:32 +0600

    home page layout done

---
+++ src/components/Video.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import classes from '../styles/Video.module.css'
+import vimage from '../assets/images/3.jpg'
+export default function Video() {
+  return (
+    <a href="quiz.html"
+            ><div className={classes.video}>
+              <img src={vimage} alt="" />
+              <p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
+              <div className={classes.qmeta}>
+                <p>10 Questions</p>
+                <p>Score : Not taken yet</p>
+              </div>
+            </div>
+          </a>
+  )
+}
